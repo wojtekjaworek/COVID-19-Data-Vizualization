52d8b4a029ebdcc5d95c37d1b5303edf9d2ac64a
Wojciech Jaworek <jaworeks@gmail.com>
2021-06-22 14:00:07 +0200

    jupyter notebook added, flask flask functionality improved

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import csv
-import scipy as sc
-import pandas as pd
-import folium
-from flask import Flask, render_template, Blueprint
-import cartopy as cp
-from first import first, html_map
+from flask import Flask, render_template, Blueprint, request, redirect, url_for
+#  from first import first, html_map
+from plots import second, create_plots, process_data, countries_list
 
+countries = countries_list()
 
 app = Flask(__name__)
-app.register_blueprint(first, url_prefix='')
+#  app.register_blueprint(first, url_prefix='')
+app.register_blueprint(second, url_prefix='')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', countries=countries)
+
+
+@app.route('/plots')
+def plots():
+    data = process_data(country=country)
+    create_plots(country=country, data=data)
+    return
+
+@app.route('/handle_data', methods=['GET', 'POST'])
+def handle_data():
+    global country
+    country = request.form['Country']
+    return redirect(url_for('plots'))
 
 if __name__=="__main__":
     app.run(debug=True)
