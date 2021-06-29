5aca45bb88ebc80590fdc259251592c1ffbedd03
Wojciech Jaworek <jaworeks@gmail.com>
2021-06-30 01:45:28 +0200

    added multiple options in html form for plotting various data, added options for world_map

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, Blueprint, request, redirect, url_for, send_file
 #  from first import first, html_map
-from website.plots import generate_plots, process_data, countries_list
+from website.plots import generate_plots, process_data, countries_list, plot_options
 from website import create_app
 from website.world_map import options_list, create_world_map
 
 countries = countries_list()
 options = options_list()
+options_for_plots = plot_options()
 
 app = create_app()
 
@@ -13,32 +14,39 @@ app = create_app()
 @app.route('/')
 def index():
 
-    world_map = create_world_map()
-    return render_template('index.html', countries=countries, options=options, world_map=world_map)
+    world_map = create_world_map(option=option_for_world_map)
+    return render_template('index.html', countries=countries, options=options, options_for_plots=options_for_plots, world_map=world_map)
 
 
 @app.route('/plots')
 def plots():
-    return render_template('plots.html', countries=countries, options=options)
+    return render_template('plots.html', countries=countries, options_for_plots=options_for_plots, options=options)
 
 @app.route('/create_plots')
 def create_plots():
     data = process_data(country=country)
-    plot = generate_plots(country=country, data=data)
+    plot = generate_plots(country=country, data=data, options=opts)
     return send_file(plot, mimetype='img/png')
+
 
 @app.route('/get_country_for_plots', methods=['GET', 'POST'])
 def get_country_for_plots():
     global country
+    global opts
+    opts = []
     country = request.form['Country']
+    opts.clear()
+    for i in range(4):
+        opts.append(request.form['Option' + str(i)])
     return redirect(url_for('plots'))
+
 
 
 @app.route('/get_option_for_map', methods=['GET', 'POST'])
 def get_option_for_map():
-
-    option = request.form['Get_option_for_map']
-    return redirect(url_for('/'))
+    global option_for_world_map
+    option_for_world_map = request.form['Get_option_for_map']
+    return redirect(url_for('index'))
 
 if __name__=="__main__":
     app.run(debug=True)
--- website/plots.py
+++ website/plots.py
@@ -7,6 +7,17 @@ from matplotlib.figure import Figure
 from io import BytesIO
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
+def plot_options():
+    data_url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+
+    data = pd.read_csv(data_url, delimiter=',', header=0, quotechar='"', encoding="utf8", parse_dates=['date'])
+    data.head()
+
+    options = data.columns.values
+
+
+    return options
+
 def countries_list():
     data_url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 
@@ -34,35 +45,35 @@ def process_data(country):
     return data
 
 
-def generate_plots(country, data):
+def generate_plots(country, data, options):
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(20, 12))
 
-    ax1.plot(data['date'], data['new_cases'])
+    ax1.plot(data['date'], data[options[0]])
     # Set title and labels for axes
-    ax1.set(xlabel="Date (YYYY-MM)", ylabel="New cases", title="New cases in " + country)
+    ax1.set(xlabel="Date (YYYY-MM)", ylabel=options[0], title=options[0]+ ' in ' + country)
     ax1.grid()
-    ax1.fill_between(data['date'], data['new_cases'], color=(185 / 255, 206 / 255, 255 / 255, 255 / 255))
+    ax1.fill_between(data['date'], data[options[0]], color=(185 / 255, 206 / 255, 255 / 255, 255 / 255))
     ax1.set_ylim(ax1.get_ylim())
 
-    ax2.plot(data['date'], data['new_deaths'], color='black')
+    ax2.plot(data['date'], data[options[1]], color='black')
     # Set title and labels for axes
-    ax2.set(xlabel="Date (YYYY-MM)", ylabel="New cases and deaths", title="New deaths in " + country)
+    ax2.set(xlabel="Date (YYYY-MM)", ylabel=options[1], title=options[1] + ' in ' + country)
     ax2.grid()
-    ax2.fill_between(data['date'], data['new_deaths'], color=(0 / 255, 0 / 255, 0 / 255, 50 / 255))
+    ax2.fill_between(data['date'], data[options[1]], color=(0 / 255, 0 / 255, 0 / 255, 50 / 255))
     ax2.set_ylim(ax2.get_ylim())
 
-    ax3.plot(data['date'], data['total_cases'])
+    ax3.plot(data['date'], data[options[2]])
     # Set title and labels for axes
-    ax3.set(xlabel="Date (YYYY-MM)", ylabel="Cumulative cases", title="Cumulative cases in " + country)
+    ax3.set(xlabel="Date (YYYY-MM)", ylabel=options[2], title=options[2] + ' in ' + country)
     ax3.grid()
-    ax3.fill_between(data['date'], data['total_cases'], color=(185 / 255, 206 / 255, 255 / 255, 255 / 255))
+    ax3.fill_between(data['date'], data[options[2]], color=(185 / 255, 206 / 255, 255 / 255, 255 / 255))
     ax3.set_ylim(ax3.get_ylim())
 
-    ax4.plot(data['date'], data['total_deaths'], color='black')
+    ax4.plot(data['date'], data[options[3]], color='black')
     # Set title and labels for axes
-    ax4.set(xlabel="Date (YYYY-MM)", ylabel="Cumulative deaths", title="Cumulative deaths in " + country)
+    ax4.set(xlabel="Date (YYYY-MM)", ylabel=options[3], title=options[3] + ' in ' + country)
     ax4.grid()
-    ax4.fill_between(data['date'], data['total_deaths'], color=(0 / 255, 0 / 255, 0 / 255, 50 / 255))
+    ax4.fill_between(data['date'], data[options[3]], color=(0 / 255, 0 / 255, 0 / 255, 50 / 255))
     ax4.set_ylim(ax4.get_ylim())
 
 
